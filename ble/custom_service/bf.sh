d27027d8d0f4305de7a29d3f22060ab5034644ff
Ariannah Black <ablack1@hubbell.com>
2024-05-28 14:12:51 -0400

    add bash script to streamline building/flashing

---
+++ ble/custom_service/bf.sh
@@ -0,0 +1,8 @@
+
+#!/bin/zsh
+
+## build/compile the script for the board in use
+west build -p always -b nrf52840dk_nrf52840 zephyr-applications/ble/custom_service
+
+## flash the board
+west flash --recover 

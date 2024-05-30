428ad89f1df7717f1a3d4fd4caea037b8d4cea64
Ariannah Black <ablack1@hubbell.com>
2024-05-30 14:23:43 -0400

    add initial code and config files

---
+++ ble/nordic_custom_service/src/main.c
@@ -0,0 +1,22 @@
+
+/* main.c */
+
+// note: needed to edit some paths in order to work with newest version of zephyr
+#include <zephyr/kernel.h>
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+
+int main(void) {
+        return 0;
+}

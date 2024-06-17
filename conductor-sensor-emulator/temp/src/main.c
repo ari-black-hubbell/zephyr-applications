c36ee7cb7bb2d0cbb48efa9ba5cd189a6817734b
Ariannah Black <ablack1@hubbell.com>
2024-06-17 14:44:28 -0400

    add structure for reference/sample code using zephyr modules necessary for the emulator

---
+++ conductor-sensor-emulator/temp/src/main.c
@@ -0,0 +1,66 @@
+
+/* main.c 
+ * a sample / test program to practice using the different zephyr modules necessary
+ * for the conductor sensor simulator. the code here may be used as reference for
+ * future projects.
+ */
+
+#include <zephyr/kernel.h>			// zephyr
+#include <zephyr/device.h>			// zephyr
+#include <stdio.h> 					// hello world
+#include <zephyr/random/random.h>	// prf
+#include <zephyr/crypto/crypto.h>	// cryptography
+
+
+/* sample uses of the randomization capabilities provided by zephyr. */
+void random_sample(void) {
+
+	/* generate random x-bit values (can be positive or negative).
+	 * not cryptographically secure.
+	 */
+	uint8_t  val8 	= sys_rand8_get();	// 8-bit value
+	uint16_t val16 	= sys_rand16_get();	// 16-bit value
+	uint32_t val32 	= sys_rand32_get();	// 32-bit value
+	uint64_t val64 	= sys_rand64_get(); // 64-bit value
+
+	// print random values
+	printk("randomly generated values: \n	\
+	  8-bit 	--> %d 		\n	\
+	  16-bit 	--> %d 		\n	\
+	  32-bit	--> %d 		\n	\
+	  64-bit	--> %lld	\n", 
+	  val8, val16, val32, val64);
+}
+
+/* sample uses of the cryptography capabilities provided by zephyr. */
+void crypto_sample(void) {
+	
+	// define necessary structures
+	struct hash_ctx ctx;		// context structure holds one time parameters
+	const struct device *dev = device_get_binding(CONFIG_CRYPTO_MBEDTLS_SHIM_DRV_NAME); // device struct for driver inst
+
+	// begin hash session (sha224, 256, 384, 512)
+	int ret = hash_begin_session(dev, &ctx, CRYPTO_HASH_ALGO_SHA256);	// sha256
+	printk("%d \n", ret);
+	if (ret == 0) {printk("initialized sha256 session\n");}
+	else {printk("failed to initialize sha256 session\n");}
+
+
+}
+
+int main(void) {
+    // hello world sample code
+	printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	// random module
+	printk("------------------------------------------------------------------\n");
+	random_sample();
+	printk("------------------------------------------------------------------\n");
+
+	// crypto module
+	printk("------------------------------------------------------------------\n");
+	crypto_sample();
+	printk("------------------------------------------------------------------\n");
+
+	return 0;
+}

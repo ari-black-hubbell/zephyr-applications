108e23fae2899eb49fed13a0fa7d105717137c1a
Ariannah Black <ablack1@hubbell.com>
2024-06-18 14:01:11 -0400

    perform hash function and print hash to console

--- conductor-sensor-emulator/temp/src/main.c
+++ conductor-sensor-emulator/temp/src/main.c
@@ -12,6 +12,7 @@
 #include <zephyr/crypto/crypto.h>	// cryptography
 
 
+
 /* sample uses of the randomization capabilities provided by zephyr. */
 void random_sample(void) {
 
@@ -36,10 +37,12 @@ void random_sample(void) {
 void crypto_sample(void) {
 	
 	// define context structure
-	struct hash_ctx ctx;					// holds one time parameters
+	struct hash_ctx ctx;								// holds one time parameters
+
 	// add some flags (crypto.h)
-	int caps = crypto_query_hwcaps(dev);	// query hw supported flags
+	// int caps = crypto_query_hwcaps(dev);				// query hw supported flags
 	ctx.flags = CAP_SYNC_OPS | CAP_SEPARATE_IO_BUFS; 
+
 	// define dev structure for driver instance
 	const struct device *dev = device_get_binding(CONFIG_CRYPTO_MBEDTLS_SHIM_DRV_NAME);
 
@@ -48,7 +51,25 @@ void crypto_sample(void) {
 	if (ret == 0) {printk("initialized sha256 session\n");}
 	else {printk("failed to initialize sha256 session\n");}
 
+	// define packet (input/output) structure
+	// struct hash_pkt pkt;
+	uint8_t hash_val[] = {0xbe};
+	uint8_t out_buf[32] = {0};			// init output buffer
+	struct hash_pkt pkt = {				// init pkt to hold input and output hash data
+		.in_buf = hash_val,				// start address of input buffer
+		.in_len = sizeof(hash_val),		// bytes to be hashed
+		.out_buf = out_buf,				// start of output buffer, can be NULL for in place ops
+	};
 
+	// perform hash function	(may not produce the correct hash?)
+	ret = hash_compute(&ctx, &pkt);
+	if (ret == 0) {printk("successfully computed hash\n", pkt.out_buf);}
+	else {printk("failed to compute hash\n");}
+
+	// output hash in hex (has 4 extra characters at the end)
+	char* cur = pkt.out_buf;
+	// prints hex form of hash with 4 extra characters at the end
+	for ( ; *cur != '\0'; ++cur) {printk("%02x", *cur);} printk("\n");
 }
 
 int main(void) {

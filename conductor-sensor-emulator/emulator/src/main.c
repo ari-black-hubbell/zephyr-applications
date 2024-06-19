cfa9e0c66a9cb35c91a605ae07adf08520e07f60
Ariannah Black <ablack1@hubbell.com>
2024-06-19 14:31:52 -0400

    minor stylistic changes, add basic functionality in main, init services code file

--- conductor-sensor-emulator/emulator/src/main.c
+++ conductor-sensor-emulator/emulator/src/main.c
@@ -3,6 +3,29 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/console/console.h>
+#include <zephyr/logging/log.h>
 
+/* Output a greeting message to the console.
+ * Returns: (int) 0 on success, != 0 on error.
+ */
+int greet(void) {
+    
+    /* write to console */ 
+    printk("\nConductor Sensor Emulator\n");
 
+    return 0;
+
+}
+
+int main(void) {
+
+    /* initializations */
+    console_init();                         /* initialize CLI */
+    int ret;                                /* for return codes */
+
+    /* greet */
+    greet();
+    
+    return 0;
+}
 
